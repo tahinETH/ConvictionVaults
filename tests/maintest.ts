d32d093c9613dfcb05cfbfa018f015c5205ee36f
chinekop <86435165+chinekop@users.noreply.github.com>
2022-04-16 22:27:46 +0300

    Update maintest.ts

--- tests/maintest.ts
+++ tests/maintest.ts
@@ -52,35 +52,3 @@ describe("createNewVault", function () {
         } */
 });
 
-/* describe("initiateTimeLock", function () {
-    before(async () => {
-        await deployConvictionBadge();
-        await deployVaultFactory();
-        const [trialNFTContractAddress, trialNFTTokenId] = await deployTestCollectible();
-    });
-
-}); */
-
-
-
-
-/* import { expect } from "chai";
-import { ethers } from "hardhat";
-
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
- */
